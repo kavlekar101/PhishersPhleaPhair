1509ebdfba83785c877d62e6e93dfec98cc3692c
kavlekar101 <kavlekar101@gmail.com>
2023-09-25 19:28:58 -0400

    initialized the components folder with the stuff from google drive and I added the App.js from the google drive.

---
+++ phishersphleaphair/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import React from "react";
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import Purchase from './components/purchase';
+import PaymentEntry from './components/paymentEntry';
+import ShippingEntry from './components/shippingEntry';
+import ViewOrder from './components/viewOrder';
+import ViewConfirmation from './components/viewConfirmation';
+
+import SampleFooter from "./components/footer";
+import Home from './components/home';
+import About from './components/about';
+import Cart from './components/cart';
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+    <Router>
+    <Navbar />
+      <div className="content">
+      <Routes>
+      <Route path='/home' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/cart' element={<Cart/>} />
+
+      <Route path='/purchase' element={<Purchase/>} />
+      <Route path="/" element={<Navigate replace to="/purchase" />} />
+      <Route path='/purchase/paymentEntry' element={<PaymentEntry/>} />
+      <Route path='/purchase/shippingEntry' element={<ShippingEntry/>} />
+      <Route path='/purchase/viewOrder' element={<ViewOrder/>} />
+      <Route path='/purchase/viewConfirmation' element={<ViewConfirmation/>} />
+      </Routes>
+      </div>
+      <SampleFooter />
+    </Router>
+    </div>
+
+
+  );
+  
+}
+
+export default App;
+
+
