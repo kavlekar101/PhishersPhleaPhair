b72b0f6e0cee5044f0b8c19cb71848e1dd77e58c
Gurbaksh <gill.577@buckeyemail.osu.edu>
2023-09-27 18:22:07 -0400

    Updated purchase

--- phishersphleaphair/src/components/purchase.js
+++ phishersphleaphair/src/components/purchase.js
@@ -2,12 +2,38 @@ import React from "react";
 
 const purchase = () => {
     let title = 'purchase';
+    const [order, setOrder] = useState({
+        buyQuanity: [0,0,0,0,0], credit_card_numer: '', expir_date: '', cvvCode: '',
+        card_holder_name: '', adress_1: '', address_2: '', city: '', state: '', zip: '',
+    });
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        navigate('/purchase/paymentEntry', {order: order, setOrder: setOrder})
+    }
+
+    console.log('order: ', order);
 
     return (
         <div>
-            <h1>
-                {title}
-            </h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="number"
+                    required
+                    onChange={(e) => 
+                        {order.buyQuanity[0] = e.target.value;}}
+                />
+                <br/>
+                <label>Product 2</label>
+                <input
+                    type="number"
+                    required
+                    onChange={(e) => 
+                        {order.buyQuanity[1] = e.target.value;}}
+                />
+                <br/>
+                <button className='button'>Pay</button>
+            </form>
         </div>
     );
 };
