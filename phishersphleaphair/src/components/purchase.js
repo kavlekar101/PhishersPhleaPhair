9f16d0776319c8ff220747d961f34137269f179a
Gurbaksh <gill.577@buckeyemail.osu.edu>
2023-09-27 17:53:46 -0400

    More setup stuff

---
+++ phishersphleaphair/src/components/purchase.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const purchase = () => {
+    let title = 'purchase';
+
+    return (
+        <div>
+            <h1>
+                {title}
+            </h1>
+        </div>
+    );
+};
+
+export default purchase;
