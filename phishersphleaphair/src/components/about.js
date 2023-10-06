bebeab092dfea1e4ddd5db79261e9c342872041c
Gurbaksh <gill.577@buckeyemail.osu.edu>
2023-10-06 14:53:24 -0400

    Added mission vision & strat. Also updated about us page to include bios and pics

--- phishersphleaphair/src/components/about.js
+++ phishersphleaphair/src/components/about.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./about.css";
 
 const About = () => {
   
@@ -7,6 +8,21 @@ const About = () => {
       <h1>
         About Us
       </h1>
+      <h2 class="names">Shashwat Rao (CEO):</h2>
+      <div>
+        <img src={require('../images/redFish.png')} width={50} height={50} alt="red" />
+        <p class="bio">I love phishing!</p>
+      </div>
+      <h2 class="names">Emil Pang (CTO):</h2>
+      <div>
+      <img src={require('../images/blueFish.png')} width={50} height={50} alt="blue" />
+        <p class="bio">I love phishing as well!</p>
+      </div>
+      <h2 class="names">Gurbaksh Gill (CFO):</h2>
+      <div>
+      <img src={require('../images/yellowFish.png')} width={100} height={50} alt="yellow" />
+        <p class="bio">Guess what! I love phishing too!</p>
+      </div>
     </div>
   );
 };
--- phishersphleaphair/src/components/home.js
+++ phishersphleaphair/src/components/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./home.css";
 
 const Home = () => {
   
@@ -13,9 +14,30 @@ const Home = () => {
       }}
     >
       <h1>Welcome to Phishers Phlea Phair!</h1>
-      <p>We sell phishes.</p>
-      <p>We sell the best phishes on the market</p>
-      <p>We are a vertically integrated </p>
+      <h2 class="home-subheadings">Mission:</h2>
+      <div class="body-text">
+        <p>
+          At Phisher's Phlea Phair, our mission is to offer a one-of-a-kind online shopping experience
+          that's as fun as it is convenient. We aim to curate a marketplace where customers can discover
+          unique and offbeat products while fostering a sense of community among buyers and sellers. We're
+          all about celebrating individuality and creativity in every purchase.
+        </p>
+      </div>
+      <h2 class="home-subheadings">Vision:</h2>
+      <div class="body-text">
+        <p>
+          Our vision for Phisher's Phlea Phair is to become the go-to destination for the unconventional and
+          the extraordinary. We strive to connect buyers with a diverse array of eccentric, handmade, and rare
+          items, all while maintaining the highest standards of security and integrity in e-commerce.
+        </p>
+      </div>
+      <h2 class="home-subheadings">Strategy:</h2>
+      <div class="body-text">
+        <p>
+          Phisher's Phlea Phair is dedicated to offering a fun and secure online marketplace for unique and quirky products,
+          fostering a vibrant community of buyers and sellers while prioritizing authenticity, sustainability, and charitable initiatives.
+        </p>
+      </div>
     </div>
   );
 };
